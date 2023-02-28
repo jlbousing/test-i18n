708431fb7989d9bfe76e1aa7dd8a08d2d3a36d82
jorge <jbousing@gmail.com>
2023-02-28 11:26:25 -0400

    traduciendo sin cambiar dinamicamente el lang

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test-i18n';
+
+  constructor(
+    private router: Router,
+    private translateService: TranslateService
+  ) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      const locale = this.router.url.split('/')[1];
+      console.log(locale);
+      if (locale) {
+        this.translateService.setDefaultLang(locale);
+        this.translateService.use(locale);
+        this.translateService.getTranslation(locale)
+          .subscribe((response: any) => console.log(response));
+          
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+import {TranslateModule, TranslateService} from '@ngx-translate/core';
 
 import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'en', pathMatch: 'full' },
+  { path: 'en', component: AppComponent, data: { locale: 'en-US' } },
+  { path: 'es', component: AppComponent, data: { locale: 'es' } }
+];
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    TranslateModule.forRoot()
   ],
-  providers: [],
+  providers: [TranslateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
